d6e82bb7643be02c81c0f070862fa3322c9aa76c
Santiago Maeda <smaeda-2019256@Kinal.edu.gt>
2024-05-21 18:58:38 -0600

    Se sube funciones de login y logout

---
+++ src/components/shared/hooks/useUserDetails.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { logout as logoutHandler } from "./useLogout.jsx";
+
+const getUserDetails = () => {
+  const userDetails = localStorage.getItem("user");
+  if (userDetails) {
+    return JSON.parse(userDetails);
+  }
+  return null;
+};
+
+export const useUserDetails = () => {
+  const [userDetails] = useState(getUserDetails());
+  const logout = () => {
+    logoutHandler();
+  };
+
+  return {
+    isLogged: Boolean(userDetails),
+    username: userDetails?.username ? userDetails.username : "Hotel",
+    logout,
+  };
+};
